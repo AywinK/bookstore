12e1d58721bd8d64bf5c6283251a63f2c15d887e
Aywin <AywinK@hotmail.com>
2024-02-05 20:33:09 +0000

    modifies autocomplete component in header component. changes label and input text colour from default to whitesmoke

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -115,7 +115,7 @@ const Header = () => {
                                 InputProps={{
                                     ...params.InputProps,
                                     style: {
-                                        color: "ghostwhite"
+                                        color: "whitesmoke"
                                     }
                                 }}
                             />
