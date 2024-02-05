292d3ecaa2ce476befde492bb8c44ed1b3bc8d25
Aywin <AywinK@hotmail.com>
2024-02-05 19:45:46 +0000

    adds nested routes with current MainLayout.jsx containing header outlet and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Homepage from './pages/Homepage'
 import { Routes, Route } from "react-router-dom";
+import MainLayout from './layouts/MainLayout';
 
 function App() {
 
@@ -15,7 +16,9 @@ function App() {
       <Header />
 
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<Homepage />} />
+        </Route>
       </Routes>
       <Footer />
     </div>
