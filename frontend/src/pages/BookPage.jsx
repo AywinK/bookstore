573a4104d4e7b0d8b6bd66e7bd8a78206b7621c6
Aywin <aywink@hotmail.co.uk>
2024-02-10 10:33:45 +0000

    sets up basic /books/:book_id page and related route

---
+++ frontend/src/pages/BookPage.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import { Container } from "@mui/material";
+
+const BookPage = () => {
+  const { book_id } = useParams();
+
+  const [bookData, setBookData] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/books/${book_id}`)
+      .then((res) => res.json())
+      .then((data) => setBookData(data))
+      .catch((err) => console.error(err));
+  }, [book_id]);
+
+  return (
+    <Container>
+      <h1>Work In Progress</h1>
+      {bookData ? JSON.stringify(bookData) : <CircularProgress />}
+    </Container>
+  );
+};
+
+export default BookPage;
