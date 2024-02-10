4d9865290102b023eac8153eab639361fc217653
Aywin <aywink@hotmail.co.uk>
2024-02-10 10:49:24 +0000

    adds link to /basket to header basket button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import BookCategory from "./pages/BookCategory";
 import ScrollToTopOnPageChange from './components/ScrollToTopOnPageChange';
 import CssBaseline from '@mui/material/CssBaseline';
 import BookPage from "./pages/BookPage";
+import Basket from "./pages/Basket";
 
 function App() {
 
@@ -15,15 +16,21 @@ function App() {
       <ScrollToTopOnPageChange />
       <Routes>
         <Route path="/" element={<MainLayout />}>
+
           <Route path="/" element={<Homepage />} />
+
           <Route path="/books">
             {/* <Route path="/books" element={?????}/> */}
             <Route path=":book_id" element={<BookPage />} />
           </Route>
+
           <Route path="/collections" >
             <Route path="/collections/" element={<Collections />} />
             <Route path=":category_name" element={<BookCategory />} />
           </Route>
+
+          <Route path="/basket" element={<Basket />} />
+
         </Route>
       </Routes>
     </CssBaseline>
