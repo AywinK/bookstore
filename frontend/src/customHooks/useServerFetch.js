ba49f8744aa7c2d9cdfd43a41b5670fb37885f17
Aywin <AywinK@hotmail.com>
2024-02-14 00:50:48 +0000

    changes custom hook and adds serverLink.json

--- frontend/src/customHooks/useServerFetch.js
+++ frontend/src/customHooks/useServerFetch.js
@@ -1,37 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
+import { serverLink } from "../siteData/serverLink.json";
 
-const useServerFetch = (urlEndpoint, options = null) => { // options for POST request
+const useServerFetch = (urlEndpoint = null, options = null) => { // options for POST request, only fetch if there is a url
 
     // initial states - no data/error while waiting for response
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    // async function implementation of fetching from backend. usecallback prevents re-render loops by stopping recreation of function
+    const fetchData = useCallback(async (isMounted) => {
+        try {
+            const response = await fetch(`${serverLink}${urlEndpoint}`, options && options);
+            if (!response.ok && isMounted) setError(`HTTPS error! Status: ${response.status}`);
+            const data = await response.json(); // parse body text of request
+            isMounted && setData(data);
+        } catch (error) {
+            isMounted && setError(error);
+        } finally {
+            isMounted && setLoading(false);
+        }
+    }, [urlEndpoint, options]);
+
+    const validEndpoint = (typeof urlEndpoint === "string")
+
     useEffect(() => {
+        console.log("rendering with valid endpoint?", validEndpoint);
 
         let isMounted = true; // flag to prevent race conditions
 
-        // async function implementation of fetching from backend
-        const fetchData = async () => {
-            try {
-                const response = await fetch(`http://192.168.1.65:3001/${urlEndpoint}`, options && options);
-                if (!response.okv && isMounted) setError(`HTTPS error! Status: ${response.status}`);
-                const data = await response.json(); // parse body text of request
-                isMounted && setData(data);
-            } catch (error) {
-                isMounted && setError(error);
-            } finally {
-                isMounted && setLoading(false);
-            }
-        };
-
-        fetchData(); // invoke fetching from backend endpoint
+        validEndpoint && fetchData(isMounted); // invoke fetching from backend endpoint only if it exists
 
         return () => {
             isMounted = false; // cleanup function when useEffect is unmounted
         };
 
-    }, [urlEndpoint, options]);
+    }, [validEndpoint, fetchData]);
 
     return { data, loading, error }; // return states from hook
 }
