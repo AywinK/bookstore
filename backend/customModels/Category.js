30243a7077abfbe02f10e709a89dc1ffaa523abe
Aywin <AywinK@hotmail.com>
2024-03-20 00:06:20 +0000

    frontend

--- backend/customModels/Category.js
+++
@@ -1,9 +0,0 @@
-
-class Category {
-    constructor(category_id, category_name) {
-        this.category_id = category_id;
-        this.category_name = category_name;
-    }
-}
-
-export { Category };
--- backend/routes/categoryRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getAllCategories } from "../controllers/categoryController.js";
-const categoryRoutes = express.Router();
-
-categoryRoutes.get("/categories", getAllCategories);
-
-export { categoryRoutes };
