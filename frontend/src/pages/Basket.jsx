b61186ea8c923df3a322d53ccb4fc143a1bda84e
Aywin <AywinK@hotmail.com>
2024-04-13 15:13:03 +0100

    adds checkout button linking to checkout/delivery

--- frontend/src/pages/Basket.jsx
+++ frontend/src/pages/Basket.jsx
@@ -1,6 +1,8 @@
 import BasketBookCard from "../components/BasketBookCard";
-import { Container } from "@mui/material";
+import Container from "@mui/material/Container";
 import { useBasket } from "../customHooks/useBasket";
+import Button from "@mui/material/Button"
+import { Link } from "react-router-dom";
 
 const Basket = () => {
 
@@ -16,11 +18,27 @@ const Basket = () => {
     >
       <h1>Basket page</h1>
       {basket.length ?
-       basket?.map((book) => (
-        <BasketBookCard key={book?.book_id} book={book} />
-      )):
-      <p>Basket is empty!</p>
-    }
+        <>
+          <Link to="/checkout/delivery">
+            <Button
+              type="button"
+              sx={{
+                borderRadius: "24px",
+                minWidth: "min(100%, 360px)",
+                fontSize: "1.25em",
+              }}
+              size="large"
+              variant="contained"
+              color="warning"
+            >Checkout
+            </Button>
+          </Link>
+          {basket?.map((book) => (
+            <BasketBookCard key={book?.book_id} book={book} />
+          ))}
+        </> :
+        <p>Basket is empty!</p>
+      }
     </Container>
   );
 };
